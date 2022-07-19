6a4d9940afdc5dab33b2d8d252255be0f15ca73a
Aswad Abbas <aabbas@stellatechnology.com>
2022-07-19 12:52:28 +0500

    Initial commit

---
+++ src/test/java/com/example/todoappeurekaserver/TodoAppEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
