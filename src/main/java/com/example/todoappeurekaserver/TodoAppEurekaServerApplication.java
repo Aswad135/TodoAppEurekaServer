6c8a8f2a0c6afcf062ec5acfd73028fe9130f8f3
Aswad Abbas <aabbas@stellatechnology.com>
2022-07-19 12:53:20 +0500

    Microservices added and Running

--- src/main/java/com/example/todoappeurekaserver/TodoAppEurekaServerApplication.java
+++ src/main/java/com/example/todoappeurekaserver/TodoAppEurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.example.todoappeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TodoAppEurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoAppEurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppEurekaServerApplication.class, args);
+    }
 
 }
